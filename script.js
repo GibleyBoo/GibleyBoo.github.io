3050a57ee3e153a5c239ca872385a14691c696ac
GibleyBoo <te15.jonas.obrell@umea.nti.se>
2017-01-26 09:35:51 +0100

    cancer

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-$(function() {
-    $("navigation").children(a).click(function()) {
-      /* Act on the event */
-      });
+$(document).ready(function() {
+    $("#navigation").children("a").click(function() {
+        $("#navigation").innerHTML = "cancer";
     });
 });
