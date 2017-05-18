5a1f60e67789d159a0dc9ab72a70dc2e2a6c8104
Ben Doverson <te15.jonas.obrell@umea.nti.se>
2017-05-18 10:19:43 +0200

    memes
    cool

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
 $(document).ready(function() {
-    $("#navigation").children("a").click(function() {
-        $("#navigation").innerHTML = "cancer";
-    });
+    // Stuff to do as soon as the DOM is ready
+    $('tab-pane div').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
 });
+});
+var january = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ];
+var february = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28 ];
+var march = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ];
+var april = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 ];
+var may = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ];
+var june = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 ];
+var july = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ];
+var august = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ];
+var september = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 ];
+var october = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ];
+var november = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 ];
+var december = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ];
+
+var months = [ january, february, march, april, may, june, july, august, september, october, november, december ];
+// March 21 - April 19
+var aries = march[21], april[19];
+// April 20 - May 20
+var taurus = april[20], may[20];
+// May 21 - June 20
+var gemini = may[21], june[20];
+// June 21 - July 22
+var cancer = june[21], july[22];
+// July 23- Aug 22
+var leo;
+// Aug 23 - Sep 22
+var virgo;
+var libra;
+var scorpio;
+var sagittarius;
+var capricorn;
+var aquarius;
+var pisces;
+var signs = [ aries, taurus, gemini, cancer, leo, virgo, libra, scorpio, sagittarius, capricorn, aquarius, pisces ]
+/*
+1.
+2.
+3.
+4.
+5.
+6. Virkring
+7. Frokost =
+8. Sort =
+9. By =
+10. Killing = Mördar
+11. Vaeske = Vätska
+
+
+
+
+jakshduiihasdjksa1290378127
+
+
+*/
